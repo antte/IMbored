211c02f3f11e891b7b11a78c287690e8062dc608
Tim Jakobsson <tim.jakobsson@gmail.com>
2011-02-10 13:29:16 +0100

    our new sweet imBored app

---
+++ imBored/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImBored::Application.config.secret_token = '858cff6c5788f1990269b00097ea93b55cb88010714fabfed9856d74422c5fe67fad3d704fd12abfd202ca31d7492c25dbeb76d52307f788799fda230d7d9571'
