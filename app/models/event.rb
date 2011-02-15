f6553ad5f2c1ca9a330d4233d8399f544463772d
Chux <lechux@gmail.com>
2011-02-15 15:07:55 +0100

    Made the api spit out some testdata but everything aint working properly

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,24 +1,13 @@
 class Event
          
     attr_accessor :title, :location, :description, :latitude, :longitude, :price, :startTime, :endTime, :id
-
-
+    
     def self.all
-      # return Api.getEvents(option)
-      APIHandler.get_events("hej")
+      APIHandler.get_events({})
     end
     
     def self.find(param)
-      return Event.new({
-        :title => "Lorum Ipsum", 
-        :location => "", 
-        :description => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", 
-        :latitude => 0, 
-        :longitude => 0, 
-        :price => 60 , 
-        :startTime => nil, 
-        :endTime => nil, 
-        :id => 1})
+        @event = APIHandler.get_event("")
     end 
     
     def initialize(attributes = {})
--- lib/api_interpreters/lastfm_api_interpreter.rb
+++ lib/api_interpreters/lastfm_api_interpreter.rb
@@ -2,6 +2,22 @@ class LastfmAPIInterpreter
 
     def get_events(options)
         events = Array.new
+
+        testevent = Event.new({
+            :title => "satan i gatan",
+            :description => "svamp i pizzan"
+        })
+
+        testevent2 = Event.new({
+            :title => "satan i gatan",
+            :description => "svamp i pizzan"
+        })
+
+        events.push(testevent)
+        events.push(testevent2)
+
+        return events
+
     end
 
 end
