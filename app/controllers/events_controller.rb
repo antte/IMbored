ee31e71224a5ce0ed08f7394686689b12e06aeed
Olle Svensson <bolliolle@gmail.com>
2011-02-15 15:08:30 +0100

    Evetn APIHandler interaction 2

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -19,7 +19,7 @@ class EventsController < ApplicationController
   # GET /events/1
   # GET /events/1.xml
   def show
-    @event = Event.find(params[:id],param[:long],param[:lati])
+    @event = Event.find(params)
 
     respond_to do |format|
       format.html # show.html.erb
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,36 +1,28 @@
 class Event
-    require '../../lib/APIHandler.rb'     
-    attr_accessor :title, :location, :description, :latitude, :longitude, :price, :startTime, :endTime, :id
+    attr_accessor :title, :location, :description, :latitude, :longitude, :price, :start_time, :end_time, :id , :timestamp
 
     def self.all(options)
-      APIHandler.get_events(options)
+        APIHandler.get_events(options)
     end
     
-    def self.find(params)
-      return Event.new({
-        :title => "Lorum Ipsum", 
-        :location => "", 
-        :description => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", 
-        :latitude => 0, 
-        :longitude => 0, 
-        :price => 60 , 
-        :startTime => nil, 
-        :endTime => nil, 
-        :id => 1})
+    def self.find(options)
+        APIHandler.get_event(options)    
     end 
     
     def initialize(attributes = {})
-      self.title = attributes[:title]
-      self.description = attributes[:description]
+        self.title = attributes[:title]
+        self.description = attributes[:description]
+        self.location = attributes[:location]
+        self.longitude = attributes[:longitude]
+        self.price = attributes[:price]
+        self.latitude = attributes[:latitude]
+        self.id = attributes[:id]
+        self.starttime = attributes[:start_time]
     end
 
     def read_attribute_for_validation(key)
-      @attributes[key]
+        @attributes[key]
     end
 
 end
 
-
-a = array.new 
-a.push = lastFMggeteventys()
-  Event.new()
