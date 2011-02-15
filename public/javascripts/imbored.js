68b92cb130f756b9f82daa4e680cec4ea6b75b80
Andreas Fliesberg <andreas.fliesberg@gmail.com>
2011-02-15 16:41:50 +0100

    Render events from json to html elements, also new datetime file to handle date conversions

--- public/javascripts/imbored.js
+++ public/javascripts/imbored.js
@@ -1,3 +1,70 @@
+/*
+ * DEPENDENCIES: datetime.js
+ */
 $(document).ready(function(){
+
+    var events_container = $("#events");
     
+    test_event = {
+        title: "Aerosmith",
+        unixtime: "1237780635",
+        description: "Aerosmith spelar i Globen i stockholm."
+    };
+
+    var test_events = [];
+    test_events.push(test_event);
+    test_events.push(test_event);
+    
+    /*
+     * Takes an object literal (and/or json??, not sure!) and converts it into a/many
+     * dom element.
+     */
+    function events_to_html(event) {
+        
+        if (event instanceof Array) {
+            var events = new Array();
+            for (e in event) {
+                events.push(events_to_html(event[e]));
+            }
+            return events;
+        }
+
+        // We append alot of stuff to this wrapping event element
+        var event_element =     $("<article>", {class: "vevent"});
+
+        var h1_element =        $("<h1>", {class: "summary"}).text(event.title);
+        var startdate_element = $("<time>", {
+                class: "dtstart", 
+                title: format_unixtime(event.unixtime, "microformat"),
+                datetime: format_unixtime(event.unixtime, "html5")
+        }).text(format_unixtime(event.unixtime, "human"));
+        
+        var description_element = $("<p>", {class: "description"}).text(event.description);
+
+        event_element.append(h1_element);
+        event_element.append(startdate_element);
+        event_element.append(description_element);
+
+        return event_element;
+
+    }
+
+    /*
+     * Jquery append items to the events_container.
+     */
+    function render_events (events, events_container) {
+        if (events instanceof Array) {
+            for (i in events) {
+                // We assume events_container is a jquery object
+                events_container.append(events[i]);
+            }
+        } else {
+            events_container.append(events);
+        }
+    }
+
+    var events = events_to_html(test_events);
+    
+    render_events(events, events_container);
+
 });
